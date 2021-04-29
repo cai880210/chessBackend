631f68c4c0a249246c33147036e2a6c47ca9ba7b
Riley Holterhus <rileyholterhus@gmail.com>
2021-04-29 12:03:13 -0400

    linux

--- build/Release/.deps/Release/obj.target/rileyChess/rileyChess.o.d
+++ build/Release/.deps/Release/obj.target/rileyChess/rileyChess.o.d
@@ -1,17 +1,17 @@
-cmd_Release/obj.target/rileyChess/rileyChess.o := g++ -o Release/obj.target/rileyChess/rileyChess.o ../rileyChess.cc '-DNODE_GYP_MODULE_NAME=rileyChess' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DNAPI_DISABLE_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/usr/include/nodejs/include/node -I/usr/include/nodejs/src -I/usr/include/nodejs/deps/openssl/config -I/usr/include/nodejs/deps/openssl/openssl/include -I/usr/include/nodejs/deps/uv/include -I/usr/include/nodejs/deps/zlib -I/usr/include/nodejs/deps/v8/include -I/mnt/c/Users/riley/OneDrive/DOcuments/Projects/Chess/nodeChessBackendcopy/node_modules/node-addon-api  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -fPIC -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/rileyChess/rileyChess.o.d.raw   -c
+cmd_Release/obj.target/rileyChess/rileyChess.o := g++ -o Release/obj.target/rileyChess/rileyChess.o ../rileyChess.cc '-DNODE_GYP_MODULE_NAME=rileyChess' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DNAPI_DISABLE_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/usr/include/nodejs/include/node -I/usr/include/nodejs/src -I/usr/include/nodejs/deps/openssl/config -I/usr/include/nodejs/deps/openssl/openssl/include -I/usr/include/nodejs/deps/uv/include -I/usr/include/nodejs/deps/zlib -I/usr/include/nodejs/deps/v8/include -I/mnt/c/Users/Riley/OneDrive/Documents/Projects/Chess/nodeChessBackendLinux/node_modules/node-addon-api  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -fPIC -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/rileyChess/rileyChess.o.d.raw   -c
 Release/obj.target/rileyChess/rileyChess.o: ../rileyChess.cc \
- /mnt/c/Users/riley/OneDrive/DOcuments/Projects/Chess/nodeChessBackendcopy/node_modules/node-addon-api/napi.h \
+ /mnt/c/Users/Riley/OneDrive/Documents/Projects/Chess/nodeChessBackendLinux/node_modules/node-addon-api/napi.h \
  /usr/include/nodejs/src/node_api.h \
  /usr/include/nodejs/src/node_api_types.h \
- /mnt/c/Users/riley/OneDrive/DOcuments/Projects/Chess/nodeChessBackendcopy/node_modules/node-addon-api/napi-inl.h \
- /mnt/c/Users/riley/OneDrive/DOcuments/Projects/Chess/nodeChessBackendcopy/node_modules/node-addon-api/napi-inl.deprecated.h \
+ /mnt/c/Users/Riley/OneDrive/Documents/Projects/Chess/nodeChessBackendLinux/node_modules/node-addon-api/napi-inl.h \
+ /mnt/c/Users/Riley/OneDrive/Documents/Projects/Chess/nodeChessBackendLinux/node_modules/node-addon-api/napi-inl.deprecated.h \
  ../main.h ../defs.h ../bitops.h
 ../rileyChess.cc:
-/mnt/c/Users/riley/OneDrive/DOcuments/Projects/Chess/nodeChessBackendcopy/node_modules/node-addon-api/napi.h:
+/mnt/c/Users/Riley/OneDrive/Documents/Projects/Chess/nodeChessBackendLinux/node_modules/node-addon-api/napi.h:
 /usr/include/nodejs/src/node_api.h:
 /usr/include/nodejs/src/node_api_types.h:
-/mnt/c/Users/riley/OneDrive/DOcuments/Projects/Chess/nodeChessBackendcopy/node_modules/node-addon-api/napi-inl.h:
-/mnt/c/Users/riley/OneDrive/DOcuments/Projects/Chess/nodeChessBackendcopy/node_modules/node-addon-api/napi-inl.deprecated.h:
+/mnt/c/Users/Riley/OneDrive/Documents/Projects/Chess/nodeChessBackendLinux/node_modules/node-addon-api/napi-inl.h:
+/mnt/c/Users/Riley/OneDrive/Documents/Projects/Chess/nodeChessBackendLinux/node_modules/node-addon-api/napi-inl.deprecated.h:
 ../main.h:
 ../defs.h:
 ../bitops.h:
